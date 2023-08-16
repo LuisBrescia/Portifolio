5e5e1c48b01ffd7e673837ec40d17835287cb134
Luis Brescia <luisfelipebrescia@gmail.com>
2023-08-16 16:12:06 -0300

    Margin-top redundante removido

--- main.js
+++ main.js
@@ -68,7 +68,7 @@ $(document).ready(function () {
         $('#enviaFormulario').text('Enviar');
         $('input[name="assunto"]').val('');
         $('textarea[name="conteudo"]').val('');
-        $('#enviaFormulario').css({'cursor': 'pointer', 'animation': 'none'});
+        $('#enviaFormulario').css({ 'cursor': 'pointer', 'animation': 'none' });
       }
     });
   });
@@ -91,6 +91,28 @@ $(document).ready(function () {
 
   // * Quando o usuário realizar um scroll, pegar o valor do scroll e alterar o menu
   $(window).scroll(function () {
+
+    // var scale = 1 + ($(window).scrollTop() / 1000); // Ajuste o divisor conforme necessário
+    // var translateX, translateY;
+
+    // if ($(window).width() >= 1400) {
+    //   translateX = -900 - ($(window).scrollTop() / 5);
+    //   translateY = -250 - ($(window).scrollTop() / 5);
+    // } else if ($(window).width() >= 1200) {
+    //   translateX = -600 - ($(window).scrollTop() / 5);
+    //   translateY = -280 - ($(window).scrollTop() / 5);
+    // } else if ($(window).width() >= 992) {
+    //   translateX = -500 - ($(window).scrollTop() / 5);
+    //   translateY = -280 - ($(window).scrollTop() / 5);
+    // } else if ($(window).width() >= 768) {
+    //   translateX = -400 - ($(window).scrollTop() / 5);
+    //   translateY = -280 - ($(window).scrollTop() / 5);
+    // } else {
+    //   translateX = -180;
+    //   translateY = -200;
+    // }
+
+    // $('.particle-container').css('transform', 'translate(' + translateX + 'px, ' + translateY + 'px) scale(' + scale + ')');
 
     if (rolagemManual) {
       console.log("retornou");
@@ -149,68 +171,68 @@ $(document).ready(function () {
   $('#xp_cefet').click(function () {
     $('.modal-title').text('CEFET-MG');
     $('.modal-body').html(
-      '<strong>1° Ano: (2020)</strong><br>' + 
+      '<strong>1° Ano: (2020)</strong><br>' +
       '<ul>' +
-        '<li>Algorítmos e lógica de programação - <span class="text-especial">C / C++</span></li>' + 
-        '<li>Programação para web - <span class="text-especial">HTML / CSS / Javascript</span></li>' +  
-        '<li>Fundamentos da informática - <span class="text-especial">Básico da computação</span></li>' + 
+      '<li>Algorítmos e lógica de programação - <span class="text-especial">C / C++</span></li>' +
+      '<li>Programação para web - <span class="text-especial">HTML / CSS / Javascript</span></li>' +
+      '<li>Fundamentos da informática - <span class="text-especial">Básico da computação</span></li>' +
       '</ul>' +
-      '<strong>2° Ano: (2021)</strong><br>' + 
+      '<strong>2° Ano: (2021)</strong><br>' +
       '<ul>' +
-        '<li>Linguagem de programação 1 - <span class="text-especial">Java</span></li>' + 
-        '<li>Aplicações para web - <span class="text-especial">PHP / Servlets</span></li>' +  
-        '<li>Organização de computadores - <span class="text-especial">Computação avançada</span></li>' + 
+      '<li>Linguagem de programação 1 - <span class="text-especial">Java</span></li>' +
+      '<li>Aplicações para web - <span class="text-especial">PHP / Servlets</span></li>' +
+      '<li>Organização de computadores - <span class="text-especial">Computação avançada</span></li>' +
       '</ul>' +
       '<strong>3° Ano: (2022)</strong><br>' +
       '<ul>' +
-        '<li>Linguagem de programação 2 - <span class="text-especial">Criação de microsseviços</span></li>' + 
-        '<li>Banco de dados - <span class="text-especial">PostgreSQL</span></li>' + 
-        '<li>Tecnologias emergentes em informática - <span class="text-especial">C++ avançado</span></li>' + 
-        '<li>Redes de Computadores - <span class="text-especial">Funcionamento / Quantificação</span></li>' +  
-        '<li>Serviços do sistema operacional - <span class="text-especial">Protocolos / Segurança</span></li>' + 
-        '<li>Projeto de sistemas - <span class="text-especial">Desenvolvimento de Software</span></li>' + 
+      '<li>Linguagem de programação 2 - <span class="text-especial">Criação de microsseviços</span></li>' +
+      '<li>Banco de dados - <span class="text-especial">PostgreSQL</span></li>' +
+      '<li>Tecnologias emergentes em informática - <span class="text-especial">C++ avançado</span></li>' +
+      '<li>Redes de Computadores - <span class="text-especial">Funcionamento / Quantificação</span></li>' +
+      '<li>Serviços do sistema operacional - <span class="text-especial">Protocolos / Segurança</span></li>' +
+      '<li>Projeto de sistemas - <span class="text-especial">Desenvolvimento de Software</span></li>' +
       '</ul>' +
       '<span>' +
-        '<strong class="fw-bold">TCC:</strong> Utilizando o aprendizado das disciplinas acima, foi desenvolvido um aplicativo para celular intitulado "Quebra-Galho",' +
-        '<a href="https://github.com/cefetmg-2022-psi-g2" class="text-especial"> clique aqui</a> para mais detalhes.<br>' +
+      '<strong class="fw-bold">TCC:</strong> Utilizando o aprendizado das disciplinas acima, foi desenvolvido um aplicativo para celular intitulado "Quebra-Galho",' +
+      '<a href="https://github.com/cefetmg-2022-psi-g2" class="text-especial"> clique aqui</a> para mais detalhes.<br>' +
       '</span>'
     );
   });
   $('#xp_puc').click(function () {
     $('.modal-title').text('PUC Minas');
     $('.modal-body').html(
-    '<i class="bi-patch-check-fill text-verde"></i>&nbsp; <span class="fw-normal">1° Período - </span>' +  
-    '<span class="text-especial">Info</span> <br>' +
-    '<i class="bi-repeat text-amarelo"></i>&nbsp; <span class="fw-normal">2° Período - </span>' +  
-    '<span class="text-especial">Info</span> <br>' +
-    '<i class="bi-dash text-vermelho"></i>&nbsp; <span class="fw-normal">3° Período - </span>' +  
-    '<span class="text-especial">Info</span> <br>' +
-    '<i class="bi-dash text-vermelho"></i>&nbsp; <span class="fw-normal">4° Período - </span>' +  
-    '<span class="text-especial">Info</span> <br>' +
-    '<i class="bi-dash text-vermelho"></i>&nbsp; <span class="fw-normal">5° Período - </span>' +  
-    '<span class="text-especial">Info</span> <br>' +
-    '<i class="bi-dash text-vermelho"></i>&nbsp; <span class="fw-normal">6° Período - </span>' +  
-    '<span class="text-especial">Info</span> <br>' +
-    '<i class="bi-dash text-vermelho"></i>&nbsp; <span class="fw-normal">7° Período - </span>' +  
-    '<span class="text-especial">Info</span> <br>' +
-    '<i class="bi-dash text-vermelho"></i>&nbsp; <span class="fw-normal">8° Período - </span>' +  
-    '<span class="text-especial">Info</span> <br><br>' +
-    'Atualmente, curso o <span class="text-especial">2° período</span> de forma regular.'
+      '<i class="bi-patch-check-fill text-verde"></i>&nbsp; <span class="fw-normal">1° Período - </span>' +
+      '<span class="text-especial">Info</span> <br>' +
+      '<i class="bi-repeat text-amarelo"></i>&nbsp; <span class="fw-normal">2° Período - </span>' +
+      '<span class="text-especial">Info</span> <br>' +
+      '<i class="bi-dash text-vermelho"></i>&nbsp; <span class="fw-normal">3° Período - </span>' +
+      '<span class="text-especial">Info</span> <br>' +
+      '<i class="bi-dash text-vermelho"></i>&nbsp; <span class="fw-normal">4° Período - </span>' +
+      '<span class="text-especial">Info</span> <br>' +
+      '<i class="bi-dash text-vermelho"></i>&nbsp; <span class="fw-normal">5° Período - </span>' +
+      '<span class="text-especial">Info</span> <br>' +
+      '<i class="bi-dash text-vermelho"></i>&nbsp; <span class="fw-normal">6° Período - </span>' +
+      '<span class="text-especial">Info</span> <br>' +
+      '<i class="bi-dash text-vermelho"></i>&nbsp; <span class="fw-normal">7° Período - </span>' +
+      '<span class="text-especial">Info</span> <br>' +
+      '<i class="bi-dash text-vermelho"></i>&nbsp; <span class="fw-normal">8° Período - </span>' +
+      '<span class="text-especial">Info</span> <br><br>' +
+      'Atualmente, curso o <span class="text-especial">2° período</span> de forma regular.'
     );
   });
   $('#xp_hackatruck').click(function () {
     $('.modal-title').text('Hackatruck');
     $('.modal-body').html(
       '<strong>Introdução:</strong><br>' +
-      'Projeto de capacitação profissional de estudantes de ' + 
-      'Instituições de Ensino Superior de Tecnologia da Informação.<br><br>' + 
+      'Projeto de capacitação profissional de estudantes de ' +
+      'Instituições de Ensino Superior de Tecnologia da Informação.<br><br>' +
       '<strong>Habilidades adquiridas:</strong><br>' +
       '<ul>' +
-        '<li>Desenvolvimento de aplicativos utilizando Swift UI</li>' + 
-        '<li>Programação de ChatBot inteligente Watson</li>' + 
-        '<li>Conhecimento em IOT</li>' + 
-        '<li>Criação de API própria</li>' +
-        '<li>Configuração de rotas utilizando Node Red</li>' + 
+      '<li>Desenvolvimento de aplicativos utilizando Swift UI</li>' +
+      '<li>Programação de ChatBot inteligente Watson</li>' +
+      '<li>Conhecimento em IOT</li>' +
+      '<li>Criação de API própria</li>' +
+      '<li>Configuração de rotas utilizando Node Red</li>' +
       '</ul>' +
       'Junto de um grupo, desenvolvi um aplicativo para IOS que auxilia pessoas na execução de exercícios físicos, ' +
       '<a href="https://github.com/LuisBrescia/Hackatruck-Academia>" class="text-especial">clique aqui</a> para mais detalhes.'
