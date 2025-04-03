ed0bd24697b3caf8cd56b72d93a04f91351bdcde
Luis Felipe Brescia <luisfelipebrescia@gmail.com>
2025-04-02 21:57:40 -0300

    feat: adicao de autoprefixer e postcss | change: blob alterado para não ficar evasivo em navegadores que não suportam blur

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,14 @@
-import { defineConfig } from 'vite';
-// import { resolve } from 'path'
+import { defineConfig } from "vite";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
-    base: '/',
-    // build: {
-    //     rollupOptions: {
-    //         input: {
-    //             main: resolve(__dirname, 'index.html'),
-    //             palavras_cruzadas: resolve(__dirname, 'HTML/palavras_cruzadas.html'),
-    //             jogo_da_memoria: resolve(__dirname, 'HTML/jogo_da_memoria.html'),
-    //             quiz: resolve(__dirname, 'HTML/quiz.html'),
-    //             questionario: resolve(__dirname, 'HTML/questionario.html'),
-    //         },
-    //     },
-    // },
-    optimizeDeps: {
-        include: ['jquery'],
+  base: "/",
+  optimizeDeps: {
+    include: ["jquery"],
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer({})],
     },
+  },
 });
